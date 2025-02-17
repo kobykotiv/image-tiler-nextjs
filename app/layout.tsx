3e22f4806f353891c905afaafbbee7ab2c8d0bc8
kobykotiv <kobykotiv@users.noreply.github.com>
2025-02-17 17:38:19 -0500

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Image Tiler',
+  description: 'Crafted with Love',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
